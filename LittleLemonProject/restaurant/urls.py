347ff08d1a3214d871feca4d39caec129511681d
huynh-kent <huynhkentd@gmail.com>
2023-08-30 13:43:04 -0700

    index view

--- LittleLemonProject/restaurant/urls.py
+++ LittleLemonProject/restaurant/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-# from .views import
+from . import views
 
 urlpatterns = [
-    path('',),
+    path('', views.index, name='index'),
 ]
