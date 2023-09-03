1b8eb743f20a74d57d1d2c0f09a32f05b55cf3ae
huynh-kent <huynhkentd@gmail.com>
2023-09-03 15:29:03 -0700

    add serializers

--- LittleLemonProject/restaurant/views.py
+++ LittleLemonProject/restaurant/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Booking
+from .serializers import BookingSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class BookingView(APIView):
+
+    def get(self, request):
+        items = Booking.objects.all()
+        serializer = BookingSerializer(items, many=True)
+        return Response(serializer.data)
+    
